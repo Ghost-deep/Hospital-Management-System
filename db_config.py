914ee6b74c8aa596701c915860990cd5b7ad2f77
Ghost-deep <deepgarai6211@gmail.com>
2025-04-20 19:52:01 +0530

    Delete db_config.py

--- db_config.py
+++
@@ -1,8 +0,0 @@
-# db_config.py
-db_config = {
-    'host': 'localhost',        
-    'user': 'root',    
-    'password': 'root', 
-    'database': 'HospitalManagement',
-    'port': 3306                
-}
